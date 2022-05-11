5a45a67cfc494c5525a753163480c8a9302cd3aa
AlexChavezz <alexischavez1234.ac@gmail.com>
2022-05-11 10:04:32 -0500

    changes

--- led_raspberry.ino
+++ led_raspberry.ino
@@ -1,15 +1,22 @@
 char value;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   pinMode(2, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   if (Serial.available()){value = Serial.read();}
-  if(value == 'A'){
+  if(value == 'A')
+  {
     digitalWrite(2, HIGH);
-  }else{
+    delay(2000);
+    digitalWrite(2, LOW);
+  }
+  else
+  {
     digitalWrite(5, HIGH);
   }
 }
