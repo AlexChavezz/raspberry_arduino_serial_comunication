55c745007f0f9e0234198f101205629c97eb8c92
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-05-11 21:16:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
+import json
+from flask import Flask, jsonify
 import serial
-import time 
+serial_port = serial.Serial('/dev/ttyUSB0', 9600)
 
-puertoSerie = serial.Serial('/dev/ttyUSB0', 9600)
+def encodeAndSend(code):
+    data = code.encode()
+    serial_port.write(data)    
 
-try:
-    while True:
-        print("Send data")
-        puertoSerie.write('A')
-        time.sleep(1)
 
-except KeyboardInterrupt:
-    print("except launch")
+server = Flask(__name__)
+@server.route("/<string:action>")
+def home(action):
+    if action == "ahead":
+        encodeAndSend('A')
+    elif action == "left":
+        encodeAndSend('L')
+    elif action == "right":
+        encodeAndSend('R')
+    elif action == "back":
+        encodeAndSend('B')
+    elif action == "stop":
+        encodeAndSend('S')
+    elif action == "frontLightsON":
+        encodeAndSend('W')
+    elif action == "frontLightsOFF":
+        encodeAndSend('Q')
+    elif action == "backLightsON":
+        encodeAndSend('E')
+    elif action == "backLightsOFF":
+        encodeAndSend('D')
+
+    return jsonify({"action": "not found"})
+        
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0")
